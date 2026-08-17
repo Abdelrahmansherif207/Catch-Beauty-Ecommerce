3463156910974df21616e1e6b52b9604f53f6dec
Abdelrahmansherif207 <abdelrahmansherif207@gmail.com>
2026-08-17 16:49:29 +0300

    Drawer: soften black separators to border-black/20

--- src/features/navigation/components/mobile/MobileFooterDrawer.tsx
+++ src/features/navigation/components/mobile/MobileFooterDrawer.tsx
@@ -64,7 +64,7 @@ export default function MobileFooterDrawer({
               "animate-in slide-in-from-left duration-300",
             )}
           >
-            <div className="flex shrink-0 items-center justify-end border-b border-black px-4 py-3">
+            <div className="flex shrink-0 items-center justify-end border-b border-black/20 px-4 py-3">
               <button
                 type="button"
                 onClick={close}
@@ -74,7 +74,7 @@ export default function MobileFooterDrawer({
                 <X className="h-5 w-5" />
               </button>
             </div>
-            <div className="flex-1 overflow-y-auto px-4 py-2 text-text-primary [&_span]:text-text-primary [&_a]:text-text-primary [&_.border-b]:border-black [&_button]:text-text-primary [&_svg]:text-text-primary">
+            <div className="flex-1 overflow-y-auto px-4 py-2 text-text-primary [&_span]:text-text-primary [&_a]:text-text-primary [&_.border-b]:border-black/20 [&_button]:text-text-primary [&_svg]:text-text-primary">
               <FooterMobileContent
                 data={data}
                 logoSrc={logoSrc}
@@ -85,7 +85,7 @@ export default function MobileFooterDrawer({
                 showCopyright={false}
               />
             </div>
-            <div className="shrink-0 border-t border-black px-4 py-3 text-text-primary [&_span]:text-text-primary">
+            <div className="shrink-0 border-t border-black/20 px-4 py-3 text-text-primary [&_span]:text-text-primary">
               <FooterContactCard contactInfo={data.contactInfo} vertical />
               {copyright && (
                 <p className="mt-2 pb-1 text-center text-xs text-text-secondary">
